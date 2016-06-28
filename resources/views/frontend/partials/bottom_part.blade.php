560c6c35bcbbb5fbec47022e72ce67242483b90f
69ideas <support@69ideas.com>
2016-06-28 16:04:08 +0500

    +right tags

--- resources/views/frontend/partials/bottom_part.blade.php
+++ resources/views/frontend/partials/bottom_part.blade.php
@@ -2,21 +2,23 @@
     &nbsp;
 </div>
 <div class="row">
-    <div class="col-md-3 col-sm-3 col-xs-12">
-        <div class="text-center sec_mid">
-            @foreach($bottom_pages as $bottom_page)
+    @foreach($bottom_pages as $bottom_page)
+        <div class="col-md-3 col-sm-3 col-xs-12">
+            <div class="text-center sec_mid">
                 @if(count($bottom_page->pages))
                     <a href="{{route ('page',$bottom_page->seo_url)}}">{{$bottom_page->menu_name}}</a>
                     <ul>
-                    @foreach($bottom_page->pages as $child)
-                            <li><a href="{{route ('page',$child->seo_url)}}" class="not_a">{{$child->menu_name}}</a></li>
-                    @endforeach
+                        @foreach($bottom_page->pages as $child)
+                            <li><a href="{{route ('page',$child->seo_url)}}" class="not_a">{{$child->menu_name}}</a>
+                            </li>
+                        @endforeach
                     </ul>
                 @elseif($bottom_page->parent_id==null and !(count($bottom_page->pages)))
                     <a href="{{route ('page',$bottom_page->seo_url)}}">{{$bottom_page->menu_name}}</a>
                     <p>{{$bottom_page->brief}}</p>
                 @endif
-            @endforeach
+            </div>
         </div>
-    </div>
+    @endforeach
+
 </div>
