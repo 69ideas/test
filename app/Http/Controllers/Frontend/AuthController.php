3fa029f998cc05e79861b512342f83b86d0394cd
69ideas <support@69ideas.com>
2016-06-28 17:53:41 +0500

    +fix auth

--- app/Http/Controllers/Frontend/AuthController.php
+++ app/Http/Controllers/Frontend/AuthController.php
@@ -17,11 +17,12 @@ class AuthController extends Controller
     public function __construct()
     {
         view()->share('active_login', 'active');
-        $top_pages=Page::where('manage_pages',1)->where('on_top',1)->get();
-        $bottom_pages=Page::where('manage_pages',1)->where('on_bottom',1)->get();
+        $top_pages = Page::where('manage_pages', 1)->where('on_top', 1)->get();
+        $bottom_pages = Page::where('manage_pages', 1)->where('on_bottom', 1)->get();
         view()->share('top_pages', $top_pages);
         view()->share('bottom_pages', $bottom_pages);
     }
+
     public function sign_in()
     {
         return view('frontend.auth');
@@ -33,22 +34,24 @@ class AuthController extends Controller
             'email' => $request->get('email'),
             'password' => $request->get('password'),
         );
-
+        $user = User::where('email', $request->get('email'))->first();
         if (\Auth::attempt($userdata, $request->get('remember-me'))) {
-            $device=Device::where('user_id', Auth::id())->where('ip',\Request::ip())->first();
-            if ($device==null){
-                $device = new Device();
-                $device->ip = \Request::ip();
-                $device->confirmed = false;
-                $device->hash = str_random(255);
-                $device->user_id = \Auth::id();
-                $device->save();
-                \Mail::queue('frontend.emails.auth', compact('request', 'email', 'device'), function (Message $message) use ($userdata) {
-                    $message->to($userdata->email)
-                        ->subject('Contact form was field');
+            $device = Device::where('user_id', Auth::id())->where('ip', \Request::ip())->first();
+            if ($device == null) {
+                \DB::transaction(function () use ($userdata, $user, $device) {
+                    $device = new Device();
+                    $device->ip = \Request::ip();
+                    $device->confirmed = false;
+                    $device->hash = str_random(255);
+                    $device->user_id = \Auth::id();
+                    $device->save();
+                    \Auth::logout();
+                    \Mail::queue('frontend.emails.repeat', compact('request', 'email', 'device', 'user'), function (Message $message) use ($user) {
+                        $message->to($user->email)
+                            ->subject('Confirm your email');
+                    });
                 });
-                $text='You are trying to log in from another location. Please check your mail and confirm its location.';
-                return view('frontend.success_registration',compact('text'));
+                return redirect()->route('repeat');
             }
             return redirect()->route('home');
         }
@@ -58,6 +61,7 @@ class AuthController extends Controller
             ->withInput($request->only('email'))
             ->with('error_message', 'These credentials do not match our records.');
     }
+
     public function sign_out()
     {
         Auth::logout();
@@ -111,8 +115,16 @@ class AuthController extends Controller
         $device->confirmed = 1;
         return redirect('/');
     }
-    public function success(){
-        $text=' You are successfully registered. Please, check your email and confirm your location';
-        return view('frontend.success_registration',compact('text'));
+
+    public function success()
+    {
+        $text = ' You are successfully registered. Please, check your email and confirm your location';
+        return view('frontend.success_registration', compact('text'));
+    }
+
+    public function repeat()
+    {
+        $text = 'You are trying to log in from another location. Please check your mail and confirm its location.';
+        return view('frontend.success_registration', compact('text'));
     }
 }
---
+++ resources/views/frontend/emails/repeat.blade.php
@@ -0,0 +1,5 @@
+Hello, {{$user->email}} !
+Don't following this link, if you didn't any actions. To activate your account, please click the following <a href="{{route('activate',$device->hash)}}"> link</a>
+
+<hr>
+We are looking forward to your consent
