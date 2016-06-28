1d63eb482e8bb122adccf0eb31dbb98c999e29f8
69ideas <support@69ideas.com>
2016-06-28 16:43:30 +0500

    no message

--- resources/views/frontend/faq.blade.php
+++ resources/views/frontend/faq.blade.php
@@ -4,7 +4,7 @@
     @foreach($faqs as $faq)
         <div class="form-group">
             <h4>{{$faq->question}}</h4>
-            {{$faq->answer}}
+            {!!  $faq->answer!!}
             <hr>
         </div>
     @endforeach
