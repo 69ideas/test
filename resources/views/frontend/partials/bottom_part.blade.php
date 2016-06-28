97b398b57d7ca9f8797e779cefe714834f485f61
69ideas <support@69ideas.com>
2016-06-28 16:39:07 +0500

    no message

--- resources/views/frontend/partials/bottom_part.blade.php
+++ resources/views/frontend/partials/bottom_part.blade.php
@@ -2,28 +2,22 @@
     &nbsp;
 </div>
 <div class="row">
-    @foreach($bottom_pages as $bottom_page)
-        @if(count($bottom_page->pages))
-            <div class="col-md-3 col-sm-3 col-xs-12">
-                <div class="text-center sec_mid">
+            @foreach($bottom_pages as $bottom_page)
+        <div class="col-md-3 col-sm-3 col-xs-12">
+            <div class="text-center sec_mid">
+                @if(count($bottom_page->pages))
                     <a href="{{route ('page',$bottom_page->seo_url)}}">{{$bottom_page->menu_name}}</a>
                     <ul>
-                        @foreach($bottom_page->pages as $child)
-                            <li><a href="{{route ('page',$child->seo_url)}}" class="not_a">{{$child->menu_name}}</a>
-                            </li>
-                        @endforeach
+                    @foreach($bottom_page->pages as $child)
+                            <li><a href="{{route ('page',$child->seo_url)}}" class="not_a">{{$child->menu_name}}</a></li>
+                    @endforeach
                     </ul>
-                </div>
-            </div>
-        @elseif($bottom_page->parent_id==null and !(count($bottom_page->pages)))
-            <div class="col-md-3 col-sm-3 col-xs-12">
-                <div class="text-center sec_mid">
+                @elseif($bottom_page->parent_id==null and !(count($bottom_page->pages)))
                     <a href="{{route ('page',$bottom_page->seo_url)}}">{{$bottom_page->menu_name}}</a>
                     <p>{{$bottom_page->brief}}</p>
-                </div>
+                @endif
             </div>
-        @endif
-
-    @endforeach
+        </div>
+            @endforeach
 
 </div>
