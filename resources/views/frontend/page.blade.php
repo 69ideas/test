a37f0734fcfaddf1eac7f54147009d5eaaf68f05
69ideas <support@69ideas.com>
2016-06-28 20:03:56 +0500

    red line

--- resources/views/frontend/page.blade.php
+++ resources/views/frontend/page.blade.php
@@ -1,6 +1,13 @@
 @extends('frontend.layout')
 @section('content')
     <h2>{{$page->title}}</h2>
-    <hr>
+    <hr style="border: none; /* Убираем границу */
+    background-color: #bb0000; /* Цвет линии */
+    color: #bb0000; /* Цвет линии для IE6-7 */
+    height: 5px;">
     {!! $page->content !!}
+    <hr style="border: none; /* Убираем границу */
+    background-color: #bb0000; /* Цвет линии */
+    color: #bb0000; /* Цвет линии для IE6-7 */
+    height: 5px;">
 @endsection
