c62461d1c8ecc853552be99686671a4e480a9d53
69ideas <support@69ideas.com>
2016-06-30 21:09:32 +0500

    fix

--- resources/views/frontend/event.blade.php
+++ resources/views/frontend/event.blade.php
@@ -9,7 +9,11 @@
             </div>
         </div>
         <div class="col-md-6">
+            @if(isset($event->image))
             <img src="/{{$event->image}}" style="width: 100%"/>
+            @else
+                <img src="/images/no-image.png" style="width: 100%"/>
+            @endif
         </div>
     </div>
     <div class="row">
