8638698ee3921ea6fef4ddef2082688fcd9ae7d7
69ideas <support@69ideas.com>
2016-07-07 18:44:49 +0500

    no message

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use PayPal\Core\PPHttpConfig;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        PPHttpConfig::$DEFAULT_CURL_OPTS = array(
+            CURLOPT_SSLVERSION => 1,
+            CURLOPT_CONNECTTIMEOUT => 10,
+            CURLOPT_RETURNTRANSFER => TRUE,
+            CURLOPT_TIMEOUT        => 60,   // maximum number of seconds to allow cURL functions to execute
+            CURLOPT_USERAGENT      => 'PayPal-PHP-SDK',
+            CURLOPT_HTTPHEADER     => array(),
+            CURLOPT_SSL_VERIFYHOST => 2,
+            CURLOPT_SSL_VERIFYPEER => 1,
+            CURLOPT_SSL_CIPHER_LIST => 'TLSv1',
+        );
     }
 }
