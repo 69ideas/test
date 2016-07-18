db2c557e693594f05d51440c5c045a68b9e34395
69ideas <support@69ideas.com>
2016-07-18 20:21:42 +0500

    sort event on member page

--- resources/views/frontend/events/index.blade.php
+++ resources/views/frontend/events/index.blade.php
@@ -25,70 +25,69 @@
 
     <div class="row">
         <div class="col-sm-12">
-            <div class="box">
-                <div class="box-body table-responsive">
-                    <table class="table table-hover table-striped">
+            <table class="searchable table table-bordered table-hover">
+                <thead>
+                <tr>
+                    <th>ID
+                        <small
+                                data-toggle="tooltip"
+                                data-placement="top"
+                                title="Click to go to event"
+                        >
+                            <i class="fa fa-info-circle"></i>
+                        </small>
+
+                    </th>
+                    <th>Short Description</th>
+                    <th>Total Collected</th>
+                    <th>Total Contributed by user</th>
+                    <th>Start Date</th>
+                    <th>Close Date</th>
+                    <th>Action</th>
+                </tr>
+                </thead>
+                <tbody>
+                @forelse($events as $event)
+                    @if (count($event->participants()->where('user_id',\Auth::user()->id)->get()) || $event->user_id==\Auth::user()->id)
                         <tr>
-                            <th>ID
-                                <small
-                                        data-toggle="tooltip"
-                                        data-placement="top"
-                                        title="Click to go to event"
-                                >
-                                    <i class="fa fa-info-circle"></i>
-                                </small>
+                            <td>
+                                @if($event->is_close)
+                                    <a href="{{ route('event.show', $event) }}"> <span
+                                                class="label label-danger"> @if (isset($event->event_number)) {{$event->event_number}} @else {{$event->id}} @endif  </span></a>
+                                @else
+                                    <a href="{{ route('event.show', $event) }}"> <span
+                                                class="label label-success">@if (isset($event->event_number)) {{$event->event_number}} @else {{$event->id}} @endif  </span></a>
+                                @endif
+                            </td>
+                            <td>{{ $event->short_description }}</td>
+                            <td>${{ number_format($event->total, 2) }}</td>
+                            <td>${{ number_format($event->current_user_collected, 2) }}</td>
+                            <td>@if(isset($event->start_date)){{ $event->start_date->format('m/d/Y')}} @endif</td>
+                            <td>@if(isset($event->closed_date)){{ $event->closed_date->format('m/d/Y')}} @else
+                                    Open @endif</td>
+                            <td>
+                                @if ($event->user_id==\Auth::user()->id)
+                                    <a href="{{ route('event.edit', $event) }}"
+                                       class="btn btn-xs btn-primary"
+                                       data-toggle="tooltip" data-placement="top"
+                                       title="Edit" style="background: #49658A;"
+                                    >
+                                        <i class="fa fa-pencil"></i>
+                                    </a>
+                                @endif
 
-                            </th>
-                            <th>Short Description</th>
-                            <th>Total Collected</th>
-                            <th>Total Contributed by user</th>
-                            <th>Start Date</th>
-                            <th>Close Date</th>
-                            <th>Action</th>
+                            </td>
                         </tr>
-                        @forelse($events as $event)
-                            @if (count($event->participants()->where('user_id',\Auth::user()->id)->get()) || $event->user_id==\Auth::user()->id)
-                                <tr>
-                                    <td>
-                                        @if($event->is_close)
-                                            <a href="{{ route('event.show', $event) }}"> <span
-                                                        class="label label-danger"> @if (isset($event->event_number)) {{$event->event_number}} @else {{$event->id}} @endif  </span></a>
-                                        @else
-                                            <a href="{{ route('event.show', $event) }}"> <span
-                                                        class="label label-success">@if (isset($event->event_number)) {{$event->event_number}} @else {{$event->id}} @endif  </span></a>
-                                        @endif
-                                    </td>
-                                    <td>{{ $event->short_description }}</td>
-                                    <td>${{ number_format($event->total, 2) }}</td>
-                                    <td>${{ number_format($event->current_user_collected, 2) }}</td>
-                                    <td>@if(isset($event->start_date)){{ $event->start_date->format('m/d/Y')}} @endif</td>
-                                    <td>@if(isset($event->closed_date)){{ $event->closed_date->format('m/d/Y')}} @else
-                                            Open @endif</td>
-                                    <td>
-                                        @if ($event->user_id==\Auth::user()->id)
-                                            <a href="{{ route('event.edit', $event) }}"
-                                               class="btn btn-xs btn-primary"
-                                               data-toggle="tooltip" data-placement="top"
-                                               title="Edit" style="background: #49658A;"
-                                            >
-                                                <i class="fa fa-pencil"></i>
-                                            </a>
-                                        @endif
-
-                                    </td>
-                                </tr>
-                            @endif
-                        @empty
-                            <tr>
-                                <td colspan="7" class="bg-info text-center text-bold">
-                                    Events not found
-                                </td>
-                            </tr>
-                        @endforelse
-                    </table>
-
-                </div>
-            </div>
+                    @endif
+                @empty
+                    <tr>
+                        <td colspan="7" class="bg-info text-center text-bold">
+                            Events not found
+                        </td>
+                    </tr>
+                @endforelse
+                </tbody>
+            </table>
         </div>
     </div>
 
